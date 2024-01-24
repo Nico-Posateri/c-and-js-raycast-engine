79b640d9e40578e57e90ac53c41231ff4c92ab0a
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-01-24 10:30:50 -0600

    Update raycast.js

--- raycasting/raycast.js
+++ raycasting/raycast.js
@@ -23,6 +23,12 @@ class Map {
     render() {
         for (var i = 0; i < MAP_NUM_ROWS; i++) {
             for (var j = 0; j < MAP_NUM_COLS; j++) {
+                var tileX = j * TILE_SIZE;
+                var tileY = i * TILE_SIZE;
+                var tileColor = this.grid[i][j] == 1 ? "#222" : #fff";
+                stroke("#222");
+                fill(tileColor);
+                rect(tileX, tileY, TILE_SIZE, TILE_SIZE);
             }
         }
     }
