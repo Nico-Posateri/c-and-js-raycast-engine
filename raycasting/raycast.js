c0ada62ac1107ee5e3ed869e9b50b05b703b54cc
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-01-24 10:10:58 -0600

    Update raycast.js

--- raycasting/raycast.js
+++ raycasting/raycast.js
@@ -1,10 +1,3 @@
-const TILE_SIZE = 32;
-const MAP_NUM_ROWS = 11;
-const MAP_NUM_COLS = 15;
-
-const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
-const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
-
 class Map {
     constructor() {
         this.grid = [
@@ -19,35 +12,19 @@ class Map {
             [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-        ];
-    }
-    render() {
-        for (var i = 0; i < MAP_NUM_ROWS; i++) {
-            for (var j = 0; j < MAP_NUM_COLS; j++) {
-                var tileX = j * TILE_SIZE; 
-                var tileY = i * TILE_SIZE;
-                var tileColor = this.grid[i][j] == 1 ? "#222" : "#fff";
-                stroke("#222");
-                fill(tileColor);
-                rect(tileX, tileY, TILE_SIZE, TILE_SIZE);
-            }
-        }
+        ]
     }
 }
 
-var grid = new Map();
-
 function setup() {
-    createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
-
+    // Initialize all objects
 }
 
 function update() {
-    // TODO: update all game objects before we render the next frame
+    // Update all game objects before next frame renders
 }
 
 function draw() {
     update();
-
-    grid.render();
+    // Render all objects frame by frame
 }
